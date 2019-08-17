880d14a6963c92b0c9db89cd653eef90617637f9
paulopr4 <paulo.pr4@hotmail.com>
2019-08-17 10:49:49 -0300

    Criei uma search bar e uma tableView

---
+++ DigitalNews/DigitalNews/Controllers/SearchVC/SearchController/SearchController.swift
@@ -0,0 +1,13 @@
+
+//
+//  File.swift
+//  DigitalNews
+//
+//  Created by paulopr4 on 17/08/19.
+//  Copyright © 2019 Giuliano Accorsi. All rights reserved.
+//
+
+import Foundation
+class SearchController {
+    
+}
